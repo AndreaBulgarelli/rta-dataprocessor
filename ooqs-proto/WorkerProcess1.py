6f51a6f3b2e8e309592a381bfa901fb87102f494
Andrea Bulgarelli <andrea.bulgarelli@inaf.it>
2024-02-13 07:44:45 +0100

    new config file for workers, adaption of test scripts and producers

--- ooqs-proto/WorkerProcess1.py
+++ ooqs-proto/WorkerProcess1.py
@@ -55,7 +55,7 @@ class WorkerProcess1(WorkerProcess):
 			# Process the decoded Avro message as needed
 			#print(self.globalname)
 			print(avro_message)
-			self.manager.send_result(avro_message)
+			#self.manager.send_result(avro_message)
 			
 		if self.supervisor.dataflowtype == "filename":
 			print(data)
