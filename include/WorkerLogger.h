f65260198e6d9bc3222f127c2aec1c8847ca09e6
lucacastaldini <luca.castaldini96@gmail.com>
2024-11-13 10:12:30 +0000

    restored files

--- include/WorkerLogger.h
+++ include/WorkerLogger.h
@@ -1,18 +1,20 @@
 #ifndef WORKERLOGGER_H
 #define WORKERLOGGER_H
-
-#include <iostream>
+ 
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/fmt/fmt.h>
 #include <string>
 #include <memory>
-
+ 
 class WorkerLogger {
 public:
     // Define a new custom log level
-    static const int SYSTEM_LEVEL_NUM = 10;
+    static const int SYSTEM_LEVEL_NUM = spdlog::level::level_enum::trace + 1;
 
-    // Constructor to initialize the WorkerLogger with a logger name and log level
-    WorkerLogger(const std::string& logger_name = "my_logger");
-
+    // Constructor to initialize the WorkerLogger with a logger name, log file, and log level
+    WorkerLogger(const std::string& logger_name = "my_logger", const std::string& log_file = "my_log_file.log", spdlog::level::level_enum level = spdlog::level::debug);
+ 
     // Logging methods for different severity levels
     void debug(const std::string& msg, const std::string& extra = "");
     void info(const std::string& msg, const std::string& extra = "");
@@ -20,13 +22,12 @@ public:
     void error(const std::string& msg, const std::string& extra = "");
     void critical(const std::string& msg, const std::string& extra = "");
     void system(const std::string& msg, const std::string& extra = "");
-
+ 
 private:
-    std::string logger_name;  // Logger name
-
+    std::shared_ptr<spdlog::logger> logger;  // Shared pointer to the spdlog logger
+ 
     // Helper method to log system-level messages
     void log_system(const std::string& msg, const std::string& extra);
 };
-
+ 
 #endif // WORKERLOGGER_H
-
